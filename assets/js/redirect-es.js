8649396ef7c116e1b441a3342215c7f503390874
unknown <jocfuentes94@gmail.com>
2024-04-21 11:53:01 -0800

    redirect es js file

---
+++ assets/js/redirect-es.js
@@ -0,0 +1,63 @@
+//automatic text write
+function typeWriter(text, elementId, speed) {
+    let index = 0;
+    const targetElement = document.getElementById(elementId);
+  
+    function type() {
+      if (index < text.length) {
+        targetElement.innerHTML += text.charAt(index);
+        index++;
+        setTimeout(type, speed);
+      }
+    }
+  
+    type();
+  }
+  
+  // Uso de la función
+  const textToType = "Gracias por contactarnos.";
+  const typingSpeed = 80; // Milisegundos entre cada letra
+  const targetElementId = "typewriter"; // ID del elemento donde se escribirá el texto
+  
+  typeWriter(textToType, targetElementId, typingSpeed);
+
+
+  //redirect seconds
+
+  function iniciarContador() {
+    let contador = 10; // Segundos
+  
+    const intervalo = setInterval(function() {
+      contador--;
+  
+      if (contador < 0) {
+        clearInterval(intervalo); // Detener el contador cuando llegue a cero
+        redireccionar(); // Redirigir después del contador
+      }
+    }, 1000); // Actualizar cada segundo
+  }
+  
+  function redireccionar() {
+    const urlDestino = "index.html"; // URL de destino
+    window.location.href = urlDestino; // Redireccionar a la URL
+  }
+  
+  // Iniciar el contador al cargar la página
+  window.onload = function() {
+    iniciarContador();
+  };
+
+  
+//<!--********************************************-->
+//<!-- ***** Developers***** -->
+//  <!--Jose Arabel Fuentes Perez
+//  contact:
+//     github> https://github.com/JoseFuentes-Dev
+//  -->
+//  <!--Claudio Benjamin Osorio
+//  contact:
+//     github> https://github.com/Claudio-Osorio/
+//  -->
+//  <!-- ***** Developers***** -->
+//  <!--********************************************-->
+  
